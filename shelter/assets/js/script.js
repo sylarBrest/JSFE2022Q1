46485b18a37627daa062212713a6460e06c3559f
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-04-22 00:49:20 +0300

    feat: add fill popup with info in js

--- shelter/assets/js/script.js
+++ shelter/assets/js/script.js
@@ -21,7 +21,39 @@ variable['nav'].addEventListener('click', closeMenu);
 variable['darken'][0].addEventListener('click', closeMenu);
 
 // Popup (modal window)
+let petsData;
+const fillData = data => petsData = data;
+
+async function getPetsInfo() {  
+  const pets = '../../assets/pets.json';
+  const res = await fetch(pets);
+  const data = await res.json(); 
+  fillData(data);
+}
+
+getPetsInfo();
+
+const fillPopup = (number) => {
+  const popupPetImage = variable['popup'].querySelector('.popup-pet-image');
+  const popupPetName = variable['popup'].querySelector('.popup-pet-name');
+  const popupPetTypeBreed = variable['popup'].querySelector('.popup-pet-typebreed');
+  const popupPetDescription = variable['popup'].querySelector('.popup-pet-description');
+  const popupPetAge = variable['popup'].querySelector('.popup-pet-age');
+  const popupPetInoculations = variable['popup'].querySelector('.popup-pet-inoculations');
+  const popupPetDiseases = variable['popup'].querySelector('.popup-pet-diseases');
+  const popupPetParasites = variable['popup'].querySelector('.popup-pet-parasites');
+  popupPetImage.src = petsData[number]['img'];
+  popupPetName.textContent = petsData[number]['name'];
+  popupPetTypeBreed.textContent = `${petsData[number]['type']} - ${petsData[number]['breed']}`;
+  popupPetDescription.textContent = petsData[number]['description'];
+  popupPetAge.textContent = petsData[number]['age'];
+  popupPetInoculations.textContent = petsData[number]['inoculations'].join(', ');
+  popupPetDiseases.textContent = petsData[number]['diseases'].join(', ');
+  popupPetParasites.textContent = petsData[number]['parasites'].join(', ');
+}
+
 const openPopup = (event) => {
+  fillPopup(event.currentTarget.dataset['pet']);
   if (event.currentTarget.classList.contains('pets-card')) {
     variable['popup'].classList.add('open');
     variable['darken'][1].classList.add('open');
@@ -30,7 +62,6 @@ const openPopup = (event) => {
 };
 
 const closePopup = (event) => {
-  console.log(event.target)
   if ((event.target.classList.contains('button-close')) || (event.target.classList.contains('darken'))) {
     variable['popup'].classList.remove('open');
     variable['darken'][1].classList.remove('open');

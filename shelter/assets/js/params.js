172f58893c4f4f7d74ffd0b9e7c047ce5584f409
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-04-22 00:02:32 +0300

    feat: add js for open/close popup

--- shelter/assets/js/params.js
+++ shelter/assets/js/params.js
@@ -3,6 +3,9 @@ const variable = {
   'nav': document.querySelector('.nav'),
   'hamburger': document.querySelector('.hamburger'),
   'logo': document.querySelector('.logo'),
-  'darken': document.querySelector('.darken'),
+  'darken': document.querySelectorAll('.darken'),
+  'pets-cards': document.querySelector('.pets-cards'),
+  'pets-card': document.querySelectorAll('.pets-card'),
+  'popup': document.querySelector('.popup'),
 }
 export default variable;
--- shelter/assets/js/script.js
+++ shelter/assets/js/script.js
@@ -8,7 +8,7 @@ const toggleMenu = () => {
 };
 
 const closeMenu = (event) => {
-  if (event.target.classList.contains('nav-link') || event.target.classList.contains('darken')) {
+  if ((event.target.classList.contains('nav-link')) || (event.target.classList.contains('darken'))) {
     variable['hamburger'].classList.remove('open');
     variable['nav'].classList.remove('open');
     variable['body'].classList.remove('open');
@@ -18,4 +18,26 @@ const closeMenu = (event) => {
 
 variable['hamburger'].addEventListener('click', toggleMenu);
 variable['nav'].addEventListener('click', closeMenu);
-variable['darken'].addEventListener('click', closeMenu);
+variable['darken'][0].addEventListener('click', closeMenu);
+
+// Popup (modal window)
+const openPopup = (event) => {
+  if (event.currentTarget.classList.contains('pets-card')) {
+    variable['popup'].classList.add('open');
+    variable['darken'][1].classList.add('open');
+    variable['body'].classList.add('open');
+  }
+};
+
+const closePopup = (event) => {
+  console.log(event.target)
+  if ((event.target.classList.contains('button-close')) || (event.target.classList.contains('darken'))) {
+    variable['popup'].classList.remove('open');
+    variable['darken'][1].classList.remove('open');
+    variable['body'].classList.remove('open');
+  }
+};
+
+variable['pets-card'].forEach(el => el.addEventListener('click', openPopup));
+variable['popup'].addEventListener('click', closePopup);
+variable['darken'][1].addEventListener('click', closePopup);

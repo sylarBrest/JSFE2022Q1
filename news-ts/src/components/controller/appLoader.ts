bea5467199c2b488ca8aa56a22e8c3db9cb33030
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-07-04 10:49:46 +0300

    feat: add footer and improvements to css

--- news-ts/src/components/controller/appLoader.ts
+++ news-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '9a018f38d1374fa1bf42264d0da880ff', // получите свой ключ https://newsapi.org/
         });
     }

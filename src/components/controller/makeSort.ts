b4194eb697ccd6a844fb45fd60c1070d640984c0
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-07-16 23:18:19 +0300

    feat: add descending sorting by name and ascending/descending sorting by year

--- src/components/controller/makeSort.ts
+++ src/components/controller/makeSort.ts
@@ -11,19 +11,43 @@ class Sorting implements Sorting {
   
   sortElements(): void {
     const sortOptions = document.getElementsByClassName('sort')[0] as HTMLSelectElement;
-    const allBikes = [...this.bikeCards] as HTMLDivElement[];
-    switch (sortOptions.selectedOptions[0]) {
-      case sortOptions.options[0]: {
-        for (const element of this.bikeCards) {
-          element.remove();
-        }
-        allBikes.sort((a, b) => ((a.dataset.bikeName?.toLowerCase() as string) < (b.dataset.bikeName?.toLowerCase() as string) ? -1 : 1));
-        document.getElementsByClassName('cards')[0].append(...allBikes);
-        break;
+    
+    const sortBy = () => {
+      const allBikes = [...this.bikeCards] as HTMLDivElement[];
+
+      for (const element of this.bikeCards) {
+        element.remove();
       }
-      default:
-        break;
+
+      switch (sortOptions.selectedOptions[0]) {
+        case sortOptions.options[0]:
+          allBikes.sort((a, b) => ((a.dataset.bikeName?.toLowerCase() as string) < (b.dataset.bikeName?.toLowerCase() as string) ? -1 : 1));
+          break;
+        case sortOptions.options[1]:
+          allBikes.sort((a, b) => ((a.dataset.bikeName?.toLowerCase() as string) > (b.dataset.bikeName?.toLowerCase() as string) ? -1 : 1));
+          break;
+        case sortOptions.options[2]:
+          allBikes.sort((a, b) => {
+            const yearA = +(a.getElementsByClassName('card-year')[0].textContent?.split(': ')[1] as string);
+            const yearB = +(b.getElementsByClassName('card-year')[0].textContent?.split(': ')[1] as string);
+            return (yearA < yearB) ? -1 : 1;
+          });
+          break;
+        case sortOptions.options[3]:
+          allBikes.sort((a, b) => {
+            const yearA = +(a.getElementsByClassName('card-year')[0].textContent?.split(': ')[1] as string);
+            const yearB = +(b.getElementsByClassName('card-year')[0].textContent?.split(': ')[1] as string);
+            return (yearA > yearB) ? -1 : 1;
+          });
+          break;
+        default:
+          break;
+      }
+
+      document.getElementsByClassName('cards')[0].append(...allBikes);
     }
+
+    sortOptions.addEventListener('change', sortBy);
   }
 }
 

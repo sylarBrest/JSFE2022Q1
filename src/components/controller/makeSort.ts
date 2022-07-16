96ef92306f5168b83358d34a437f3d49041a650f
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-07-16 23:01:37 +0300

    feat: add sorting of elements by name in ascending order

--- src/components/controller/makeSort.ts
+++ src/components/controller/makeSort.ts
@@ -3,6 +3,28 @@ interface Sorting {
 }
 
 class Sorting implements Sorting {
+  private bikeCards;
+
+  constructor() {
+    this.bikeCards = document.getElementsByClassName('card');
+  }
+  
+  sortElements(): void {
+    const sortOptions = document.getElementsByClassName('sort')[0] as HTMLSelectElement;
+    const allBikes = [...this.bikeCards] as HTMLDivElement[];
+    switch (sortOptions.selectedOptions[0]) {
+      case sortOptions.options[0]: {
+        for (const element of this.bikeCards) {
+          element.remove();
+        }
+        allBikes.sort((a, b) => ((a.dataset.bikeName?.toLowerCase() as string) < (b.dataset.bikeName?.toLowerCase() as string) ? -1 : 1));
+        document.getElementsByClassName('cards')[0].append(...allBikes);
+        break;
+      }
+      default:
+        break;
+    }
+  }
 }
 
 export default Sorting;

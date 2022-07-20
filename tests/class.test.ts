5c0b8a9a087d3300cd424467948d9d480153fd8a
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-07-21 00:57:38 +0300

    refactor: add tests

--- tests/class.test.ts
+++ tests/class.test.ts
@@ -1,13 +1,21 @@
 import AppView from '../src/components/view/appView';
 import BikeStorage from '../src/components/controller/bikeStorage';
+import Filters from '../src/components/controller/applyFilters';
 
 describe('Class objects', () => {
-  it('should be undefined', () => {
+  it('should be instance of class', () => {
     expect(new AppView()).toBeInstanceOf(AppView);
+  });
+
+  it('should be instance of class', () => {
+    expect(new BikeStorage()).toBeInstanceOf(BikeStorage);
   });
 
   it('should be defined', () => {
     expect(new BikeStorage().getBikesFromStorage()).toBeDefined();
   });
 
+  it('should be undefined', () => {
+    expect(new Filters().resetFilters()).toBeUndefined();
+  });
 });
--- tests/draw.test.ts
+++ tests/draw.test.ts
@@ -1,8 +1,10 @@
 import Footer from '../src/components/view/footer/footer';
 import Header from '../src/components/view/header/header';
+import Card from '../src/components/view/main/cards/card/card';
+import Reset from '../src/components/view/main/filters/reset/reset';
 
 describe('Draw Components', () => {
-  it('should drawHeader', () => {
+  it('should draw header', () => {
     const header = document.createElement('header');
     header.className = 'header';
     document.body.append(header);
@@ -12,13 +14,33 @@ describe('Draw Components', () => {
     expect(result).toContain(fragment);
   });
 
-  it('should drawFooter', () => {
+  it('should draw footer', () => {
     const footer = document.createElement('footer');
     footer.className = 'footer';
     document.body.append(footer);
     new Footer().drawFooter();
     const result = document.getElementsByClassName('footer')[0].innerHTML;
     const fragment = `<div class="container footer-container"><div class="footer-data"><span class="copy-year">© 2022</span><span class="github"><a class="github-link" href="https://github.com/sylarBrest" target="_blank" rel="noopener noreferrer">sylarBrest</a></span></div><a class="rss-logo" href="https://rs.school/js-stage0/" target="_blank" rel="noopener noreferrer" title="Rolling Scope School"></a></div>`;
+    expect(result).toContain(fragment);
+  });
+
+  it('should draw card', () => {
+    const cards = document.createElement('div');
+    cards.className = 'cards';
+    document.body.append(cards);
+    new Card().drawCard();
+    const result = document.getElementsByClassName('cards')[0].innerHTML;
+    const fragment = `<div class="card" data-bike-num="18"><h2 class="card-name">AIST Tracker 2.0</h2><img class="card-image" alt="AIST Tracker 2.0" src="./assets/images/aist-tracker.webp"><div class="card-info-all"><div class="card-info"><p class="card-manufacturer">Производитель: AIST</p><p class="card-year">Модельный год: 2018</p><p class="card-category">Категория: Городской</p><p class="card-wheel-size">Размер колёс: 26"</p><p class="card-frame-size">Размер рамы: S</p><p class="card-color">Цвет: оранжевый</p><p class="card-stock-amount">Количество на складе: 20</p><p class="card-popular">Популярный: да</p></div><div class="card-bag"></div></div></div>`;
+    expect(result).toContain(fragment);
+  });
+
+  it('should draw reset button', () => {
+    const filters = document.createElement('div');
+    filters.className = 'filters';
+    document.body.append(filters);
+    new Reset().drawResetButtons();
+    const result = document.getElementsByClassName('filters')[0].innerHTML;
+    const fragment = `<button class="reset-button reset-filters-button">Сбросить фильтры</button>`;
     expect(result).toContain(fragment);
   });
 });

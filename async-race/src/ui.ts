a8d4d964d39cf4ff3edcccb426d0f4a1a9ac98b9
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-08-09 14:58:42 +0300

    feat: add disabling/enabling buttons while race

--- async-race/src/ui.ts
+++ async-race/src/ui.ts
@@ -309,11 +309,65 @@ const saveWinner: SpecifiedPromiseFn<WinnerResult, void> = async (
   }
 };
 
+const disableButtons = (): void => {
+  const raceButton = <HTMLButtonElement>document.getElementsByClassName('race-button')[0];
+  const createCarButton = <HTMLButtonElement>document.getElementsByClassName('create-car-button')[0];
+  const winnersButton = <HTMLButtonElement>document.getElementsByClassName('winners-button')[0];
+  const generateCarsButton = <HTMLButtonElement>document.getElementsByClassName('generate-cars-button')[0];
+  const selectButtons = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName('select-button');
+  const removeButtons = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName('remove-button');
+  const prevButton = <HTMLButtonElement>document.getElementsByClassName('prev-button')[0];
+  const nextButton = <HTMLButtonElement>document.getElementsByClassName('next-button')[0];
+
+  Array.from(selectButtons).forEach((selectButton: HTMLButtonElement) => {
+    const currentButton = selectButton;
+    currentButton.disabled = true;
+  });
+
+  Array.from(removeButtons).forEach((removeButton: HTMLButtonElement) => {
+    const currentButton = removeButton;
+    currentButton.disabled = true;
+  });
+
+  raceButton.disabled = true;
+  createCarButton.disabled = true;
+  winnersButton.disabled = true;
+  generateCarsButton.disabled = true;
+  prevButton.disabled = true;
+  nextButton.disabled = true;
+};
+
+const enableButtons = (): void => {
+  const createCarButton = <HTMLButtonElement>document.getElementsByClassName('create-car-button')[0];
+  const winnersButton = <HTMLButtonElement>document.getElementsByClassName('winners-button')[0];
+  const generateCarsButton = <HTMLButtonElement>document.getElementsByClassName('generate-cars-button')[0];
+  const selectButtons = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName('select-button');
+  const removeButtons = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName('remove-button');
+  const resetButton = <HTMLButtonElement>document.getElementsByClassName('reset-button')[0];
+
+  Array.from(selectButtons).forEach((selectButton: HTMLButtonElement) => {
+    const currentButton = selectButton;
+    currentButton.disabled = false;
+  });
+
+  Array.from(removeButtons).forEach((removeButton: HTMLButtonElement) => {
+    const currentButton = removeButton;
+    currentButton.disabled = false;
+  });
+
+  createCarButton.disabled = false;
+  winnersButton.disabled = false;
+  generateCarsButton.disabled = false;
+  resetButton.disabled = false;
+
+  Utils.prevButtonUpdateState();
+  Utils.nextButtonUpdateState();
+};
+
 const racing: SpecifiedPromiseFn<SpecifiedPromiseFn<number, RaceResult>, WinnerResult> = async (
   action: SpecifiedPromiseFn<number, RaceResult>,
 ): Promise<WinnerResult> => {
-  const raceButton = <HTMLButtonElement>document.getElementsByClassName('race-button')[0];
-  raceButton.disabled = true;
+  disableButtons();
 
   const promises: Promise<RaceResult>[] = storage.garage.map(({ id }) => action(id));
   const winner: WinnerResult = await raceAll(promises, storage.garage.map((car: Car) => car.id));
@@ -323,8 +377,7 @@ const racing: SpecifiedPromiseFn<SpecifiedPromiseFn<number, RaceResult>, WinnerR
 
   saveWinner(winner);
 
-  const resetButton = <HTMLButtonElement>document.getElementsByClassName('reset-button')[0];
-  resetButton.disabled = false;
+  enableButtons();
 
   return winner;
 };

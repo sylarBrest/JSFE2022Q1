d483293792e992e4c5fe941af739ce4645c9efdb
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-08-17 01:10:54 +0300

    feat: destruct event.target.classList and use switch instead of ifs

--- async-race/src/ui.ts
+++ async-race/src/ui.ts
@@ -4,53 +4,63 @@ import { ESortingBy } from './types';
 export default function listeners(): void {
   document.body.addEventListener('click', (event: MouseEvent) => {
     if (event.target instanceof HTMLButtonElement) {
-      if (event.target.classList.contains('garage-button')) {
-        Ui.switchToGarageView();
-      }
-      if (event.target.classList.contains('winners-button')) {
-        Ui.switchToWinnersView();
-      }
-      if (event.target.classList.contains('create-car-button')) {
-        Ui.addNewCar();
-      }
-      if (event.target.classList.contains('select-button')) {
-        Ui.prepareUpdate(+event.target.dataset.carSelectId);
-      }
-      if (event.target.classList.contains('remove-button')) {
-        Ui.removeSelectedCar(+event.target.dataset.carRemoveId);
-      }
-      if (event.target.classList.contains('update-car-button')) {
-        Ui.updateSelectedCar(Ui.getSelectedCarId());
-      }
-      if (event.target.classList.contains('race-button')) {
-        Ui.racing(Ui.carStarting);
-      }
-      if (event.target.classList.contains('reset-button')) {
-        Ui.resetting();
-      }
-      if (event.target.classList.contains('generate-cars-button')) {
-        Ui.generateCars();
-      }
-      if (event.target.classList.contains('start-button')) {
-        Ui.carStarting(+event.target.dataset.carStartId);
-      }
-      if (event.target.classList.contains('stop-button')) {
-        Ui.carStopping(+event.target.dataset.carStopId);
-      }
-      if (event.target.classList.contains('next-button')) {
-        Ui.nextPage();
-      }
-      if (event.target.classList.contains('prev-button')) {
-        Ui.prevPage();
+      const buttonClassList = event.target.classList;
+      switch (true) {
+        case buttonClassList.contains('garage-button'):
+          Ui.switchToGarageView();
+          break;
+        case buttonClassList.contains('winners-button'):
+          Ui.switchToWinnersView();
+          break;
+        case buttonClassList.contains('select-button'):
+          Ui.prepareUpdate(+event.target.dataset.carSelectId);
+          break;
+        case buttonClassList.contains('remove-button'):
+          Ui.removeSelectedCar(+event.target.dataset.carRemoveId);
+          break;
+        case buttonClassList.contains('create-car-button'):
+          Ui.addNewCar();
+          break;
+        case buttonClassList.contains('update-car-button'):
+          Ui.updateSelectedCar(Ui.getSelectedCarId());
+          break;
+        case buttonClassList.contains('race-button'):
+          Ui.racing(Ui.carStarting);
+          break;
+        case buttonClassList.contains('reset-button'):
+          Ui.resetting();
+          break;
+        case buttonClassList.contains('generate-cars-button'):
+          Ui.generateCars();
+          break;
+        case buttonClassList.contains('start-button'):
+          Ui.carStarting(+event.target.dataset.carStartId);
+          break;
+        case buttonClassList.contains('stop-button'):
+          Ui.carStopping(+event.target.dataset.carStopId);
+          break;
+        case buttonClassList.contains('next-button'):
+          Ui.nextPage();
+          break;
+        case buttonClassList.contains('prev-button'):
+          Ui.prevPage();
+          break;
+        default:
+          break;
       }
     }
 
     if (event.target instanceof HTMLTableCellElement) {
-      if (event.target.classList.contains('wins-sort')) {
-        Ui.sortWinners(ESortingBy.wins);
-      }
-      if (event.target.classList.contains('time-sort')) {
-        Ui.sortWinners(ESortingBy.time);
+      const tHeadClassList = event.target.classList;
+      switch (true) {
+        case tHeadClassList.contains('wins-sort'):
+          Ui.sortWinners(ESortingBy.wins);
+          break;
+        case tHeadClassList.contains('time-sort'):
+          Ui.sortWinners(ESortingBy.time);
+          break;
+        default:
+          break;
       }
     }
   });
